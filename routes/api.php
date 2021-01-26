2168dca0c7cc06e422bcbb4163580695f1ee2539
Julio Cesar <juliocsrmf@gmail.com>
2021-01-26 13:49:47 -0300

    Upload completo de arquivos via API

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,4 +19,32 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('upload', function(Request $request) {
+    $array = ['error' => ''];
+    $rules = [
+        'name' => ['required', 'min:2'],
+        'foto' => ['mimes:jpg,png', 'file', 'required']
+    ];
 
+    $validator = Validator::make($request->all(), $rules);
+
+    if($validator->fails()){
+        $array['error'] = $validator->messages();
+        return $array;
+    }
+
+    if($request->hasFile('foto')) {
+        if($request->file('foto')->isValid()){
+            $foto = $request->file('foto')->store('public');
+            $url = asset(Storage::url($foto));
+            $array['foto'] = $url;
+            return $array;
+        } else {
+            $array['error'] = 'Arquivo corrompido durante upload';
+        }
+    } else {
+        $array['error'] = 'Não foi enviado arquivo';
+    }
+
+    return $array;
+});
